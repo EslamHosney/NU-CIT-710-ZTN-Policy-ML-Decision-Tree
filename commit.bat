c6e51cc1988f86f403cb029dd32d6f13d03ccbfe
Eslam Hosney <eslamsamymeg@gmail.com>
2021-12-31 17:30:21 +0200

    Test Commit fix

--- commit.bat
+++ commit.bat
@@ -1,6 +1,16 @@
 ECHO Start
 git add .
-git commit -a -m "Buff Update"
+
+
+IF %1.==. GOTO NoCommittxt
+set arg1=%1
+git commit -a -m %arg1%
+
+GOTO End1
+
+:NoCommittxt
+  git commit -a -m "Buff Update"
+GOTO End1
+:End1
 git push
 ECHO Pushed
-#PAUSE
